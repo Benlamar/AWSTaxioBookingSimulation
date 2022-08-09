f74e0236226bb5c5630d81d62ba578ab460fe564
benlamar <benlmr18@gmail.com>
2022-08-09 10:49:05 +0530

    uploading initial code, taxi regitration and coordinates generation

---
+++ updateLocation.py
@@ -0,0 +1,23 @@
+import random
+from pymongo import MongoClient
+import datetime
+
+def random_location():
+    x_lad = random.uniform(28.55195,28.75195)
+    y_long = random.uniform(77.13149,77.33149)
+    return [x_lad,y_long]
+        
+#database
+db_uri = 'mongodb://localhost:27017'
+mongo_connect = MongoClient(db_uri)
+
+db = mongo_connect.taxi_customer_db
+taxi_col = db.taxis
+res = list(taxi_col.find())
+
+for obj in taxi_col.find():
+    taxi_col.update_one({'id':obj['id']}, {'$set':{'timestamp':datetime.datetime.now()}})
+    taxi_col.update_one({'id':obj['id']}, {'$set':{'location':{'coordinates':random_location()}}})
+    print('updating ...')
+
+
